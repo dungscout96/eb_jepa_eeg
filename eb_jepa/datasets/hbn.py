352a13d3369d560627b888ce884cb8e89deec1e7
Dung Truong <dt.young112@gmail.com>
2026-02-21 08:57:51 -0800

    correct windowing for movie probe

--- eb_jepa/datasets/hbn.py
+++ eb_jepa/datasets/hbn.py
@@ -49,11 +49,21 @@ def get_window_movie_metadata(
         movie: str, 
         movie_features: pd.DataFrame
     ):
+    '''
+    Window onset is with respect to the start of movie recording, not the start of the trial. 
+    We assume that the movie starts at the same time as the recording, and that there are no dropped frames in the movie.
+    '''
     # compute the movie timestamp corresponding to the window onset
     movie_timestamp = window_onset / sfreq
     # compute the corresponding frame index in the movie
     frame_index = int(movie_timestamp * MOVIE_METADATA[movie]["fps"])
-    features = movie_features.iloc[frame_index]
+    # assume visual processing of EEG still happening for 2 seconds after the movie ends, 
+    # so we can still use the features of the last frame for windows that are within 2 seconds after the movie ends. 
+    # This is important for handling cases where the recording is slightly longer than the movie due to annotation imprecision.
+    if (frame_index - MOVIE_METADATA[movie]["frame_count"]) < (MOVIE_METADATA[movie]["fps"] * 2):
+        frame_index = len(movie_features) - 1
+    # frame_index = min(frame_index, len(movie_features) - 1)
+    features = movie_features.iloc[frame_index].to_dict()
     return features
 
 def load_or_download(release, dataset_name):
@@ -72,7 +82,7 @@ def load_or_download(release, dataset_name):
         dataset.download_all(n_jobs=-1)
     return dataset
 
-def get_movie_recording_duration(raw: mne.io.BaseRaw):
+def get_movie_recording_duration(raw: mne.io.BaseRaw, movie:str):
     events, event_id = mne.events_from_annotations(raw)
     # compute the duration from start to stop
     events_filtered = mne.pick_events(
@@ -81,25 +91,27 @@ def get_movie_recording_duration(raw: mne.io.BaseRaw):
     )
     duration_sample = events_filtered[1, 0] - events_filtered[0, 0]
     duration_seconds = duration_sample / raw.info["sfreq"]
+    if duration_seconds > MOVIE_METADATA[movie]["duration"] + 60:
+        raise ValueError(f"Recording has duration {duration_seconds}s (+60s) for movie '{movie}', which is unexpected. Please check the recording.")
     return duration_seconds
 
-def preprocess_dataset(dataset):
-    from braindecode.preprocessing import create_fixed_length_windows
+def reject_recording(raw: mne.io.BaseRaw, movie:str):
+    # reject recording that don't have viedo_start and video_stop annotations
+    events, event_id = mne.events_from_annotations(raw)
+    if "video_start" not in event_id or "video_stop" not in event_id:
+        return True
 
-    windowed = create_fixed_length_windows(
-        dataset,
-        window_size_samples=1000,  # e.g., 1000 for ~2s at 500Hz
-        window_stride_samples=500,
-        drop_last_window=True,
-    )
-    return windowed
+    # reject recording that have duration less than movie duration
+    duration_seconds = get_movie_recording_duration(raw, movie)
+    if duration_seconds < MOVIE_METADATA[movie]["duration"] - 0.5: # allow 0.5s tolerance for annotation imprecision
+        return True
 
 class HBNDataset(Dataset):
     """Dataset where each item is a random crop of n_windows contiguous windows (WxCxT).
     TODO: revisit fixed window number
     """
 
-    def __init__(self, split="train", n_windows=16):
+    def __init__(self, split="train", n_windows=16, window_size_seconds=2):
         if split == "train":
             releases = TRAIN_RELEASES
         elif split == "val":
@@ -110,10 +122,17 @@ class HBNDataset(Dataset):
             raise ValueError(f"Invalid split '{split}'. Must be 'train', 'val', or 'test'.")
 
         self.n_windows = n_windows
+        self.window_size_seconds = window_size_seconds
         self.recordings = []
         for release, dataset_name in releases.items():
             dataset = load_or_download(release, dataset_name)
-            windowed_dataset = preprocess_dataset(dataset)
+            from braindecode.preprocessing import create_fixed_length_windows
+            windowed_dataset = create_fixed_length_windows(
+                dataset,
+                window_size_samples=int(window_size_seconds*dataset.datasets[0].raw.info["sfreq"]),
+                window_stride_samples=int(window_size_seconds*dataset.datasets[0].raw.info["sfreq"]), # non-overlapping windows
+                drop_last_window=True,
+            )
             for recording_ds in windowed_dataset.datasets:
                 if len(recording_ds) >= n_windows:
                     self.recordings.append(recording_ds)
@@ -135,3 +154,78 @@ class HBNDataset(Dataset):
             for i in range(self.n_windows)
         ])
         return windows
+
+class HBNMovieProbeDataset(Dataset):
+    """Dataset where each window is associated with the movie features at the corresponding timestamp.
+    """
+
+    def __init__(self, split="train", window_size_seconds=2):
+        self.window_size_seconds = window_size_seconds
+        if split == "train":
+            releases = TRAIN_RELEASES
+        elif split == "val":
+            releases = VAL_RELEASES
+        elif split == "test":
+            releases = TEST_RELEASES
+        else:
+            raise ValueError(f"Invalid split '{split}'. Must be 'train', 'val', or 'test'.")
+
+        data = []
+        for release, dataset_name in releases.items():
+            dataset = load_or_download(release, dataset_name)
+            data.append(dataset)
+        data = BaseConcatDataset(data)
+
+        selected_recordings = []
+        rejected = 0
+        for recording_ds in data.datasets: 
+            raw = recording_ds.raw
+
+            for idx, ann in enumerate(raw.annotations):
+                # set the duration of the "video_start" annotation to the actual movie recording duration, which can be computed from the "video_start" and "video_stop" annotations. This is important for correctly creating windows that align with the movie timestamps.
+                if ann["description"] == "video_start":
+                    movie_recording_duration = get_movie_recording_duration(raw, movie=TASK)
+                    # clipping duration to movie duration, while avoiding extremely long recordings that may be due to annotation errors
+                    movie_recording_duration = min(movie_recording_duration, MOVIE_METADATA[TASK]["duration"])
+                    print(f"Setting duration of 'video_start' annotation to {movie_recording_duration:.2f}s for recording '{recording_ds}'")
+                    raw.annotations.duration[idx] = movie_recording_duration
+
+            if reject_recording(raw, movie=TASK):
+                rejected += 1
+                continue
+            selected_recordings.append(recording_ds)
+        print(f"Rejected {rejected}/{len(data.datasets)} recordings")
+        data = BaseConcatDataset(selected_recordings)
+        from braindecode.preprocessing import create_windows_from_events
+        sfreq = data.datasets[0].raw.info["sfreq"]
+        windowed = create_windows_from_events(
+            data,
+            mapping={"video_start": 0},
+            trial_stop_offset_samples=-int(0.1*sfreq), # just one big trial for the whole movie
+            window_size_samples=int(window_size_seconds*sfreq),
+            window_stride_samples=int(window_size_seconds*sfreq), # non-overlapping windows
+            drop_last_window=True,
+        )
+        self.data = windowed
+        
+        self.sfreq = data.datasets[0].raw.info["sfreq"]
+        self._preload_movie_features()
+
+    def _preload_movie_features(self):
+        self.movie_features = {
+            "ThePresent": pd.read_csv(MOVIE_METADATA["ThePresent"]["feature_csv"]),
+        }
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        X, y, crop_inds = self.data[idx]
+        window_onset = crop_inds[1]  # i_start_in_trial (samples)
+        features = get_window_movie_metadata(
+            window_onset=window_onset,
+            sfreq=self.sfreq,
+            movie=TASK,
+            movie_features=self.movie_features[TASK],
+        )
+        return torch.from_numpy(X).float(), features
+
--- tests/test_eeg_probe.py
+++ tests/test_eeg_probe.py
@@ -1,17 +1,20 @@
 from braindecode.models import ShallowFBCSPNet
-from eb_jepa.eeg_probe_decoder import HBNMovieProbeDataset
+from eb_jepa.datasets.hbn import HBNMovieProbeDataset
 from torch.utils.data import DataLoader
 from torch.functional import F
 
 def test_hbn_movie_probe_dataset():
-    dataset = HBNMovieProbeDataset(split="train")
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+    window_size_seconds = 2
+    window_size_samples = int(window_size_seconds * 100)  # example for 100Hz sampling rate
+    dataset = HBNMovieProbeDataset(split="train", window_size_seconds=window_size_seconds)
+    batch_size = 64
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     selected = "luminance_mean"  # example feature name from the movie metadata
     model = ShallowFBCSPNet(
         n_chans=129,  # example number of EEG channels
         n_outputs=1,   # regression task
-        n_times=1000,  # example number of time points in each window
+        n_times=window_size_samples,  # example number of time points in each window
     )
     for batch in dataloader:
         # batch is a tuple of (X, features), where:
@@ -21,15 +24,14 @@ def test_hbn_movie_probe_dataset():
         windows = X  # (B, C, T)
         movie_features = features[selected] # (B, F)
 
-        assert windows.shape[0] == movie_features.shape[0] == 4
-        assert windows.shape[1:] == (129, 1000)  # example shape
+        assert len(windows) == len(movie_features)
+        assert windows.shape[1:] == (129, window_size_samples)  # example shape
 
         # Forward pass through the model (example)
         outputs = model(windows)
-        assert outputs.shape == (4, 1)  # example output shape for regression
+        assert outputs.ndim == 2 and outputs.shape[1] == 1  # example output shape for regression
         loss = F.mse_loss(outputs.squeeze(), movie_features)
         print(f"Loss: {loss.item()}")
-        break  # just test one batch
 
         
 
